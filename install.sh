a553a9de9efd18a30fd581278fd1be4163fecc33
Devin <devin.lai@ichef.com.tw>
2018-11-14 17:07:11 +0800

    Update Install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,25 @@ install_ansible() {
 }
 
 install_ansible
+
+rm -rf mac_setup
+mkdir mac_setup
+
+curl -L http://github.com/iCHEF/iOS-Dev-Playbook/zipball/master/ > mac_setup/master.zip
+cd mac_setup
+unzip -a master.zip
+
+TARGET_FOLDER=pwd
+for file in ./*; do
+  if [[ $file =~ "iOS-Dev-Playbook" ]]; then
+    TARGET_FOLDER=$file
+  fi
+done
+
+cd $TARGET_FOLDER
 ansible-galaxy install -r requirements.yml
 ansible-playbook main.yml -i inventory -K
+
+
+cd ../..
+rm -rf mac_setup
